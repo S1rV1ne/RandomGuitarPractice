fce26ffef959d58dda88200694b75d6ad6468589
sparky97865 <theboss97865@gmail.com>
2022-03-13 23:43:37 -0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import {readline, guitarSrings, ask, naturalNotes, sharpNotes, flatNotes, ttNotesS, ttNotesF, sNote, fNote, nNote, ttNoteS, ttNoteF, RanNNote, RanFNote, RanSNote, RanTTNoteS, RanTTNoteF} from './GeneralFunctions.js';
 (async function start() {
 let answerWhatToPractice = await ask("What do you wish to practice? ");
-let answerWhatToPractice = st
-
+//let answerWhatToPractice = 
+console.log(guitarSrings);
 
 
 })();
